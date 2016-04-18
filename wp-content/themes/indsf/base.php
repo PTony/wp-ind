684f063e3e5630c5cc89f9620d5349097f793e67
Dessaint <franck.dessaint@gmail.com>
2016-04-18 18:00:25 +0200

    Premier contenus

--- wp-content/themes/indsf/base.php
+++ wp-content/themes/indsf/base.php
@@ -8,7 +8,7 @@ Template Name: Base
 
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <div class=".container__header .header .header-title"><h2><?php the_title(); ?></h2></div>
+    <h3><?php the_title(); ?></h3>
 
     <div>
 
@@ -22,3 +22,18 @@ Template Name: Base
 
 <?php get_footer(); ?>
 
+</div>
+	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.js"></script>
+
+    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    	<script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
+		<div id="fb-root"></div>
+		<script>(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.5";
+		fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+	</body>
+</html>
