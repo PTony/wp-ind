b29d9db0076b3b5b6429e60e8fa2384074bebb94
Dessaint <franck.dessaint@gmail.com>
2016-04-18 17:47:59 +0200

    Ajout de l'actualité dans le menu école

--- wp-content/themes/indsf/base.php
+++ wp-content/themes/indsf/base.php
@@ -6,5 +6,19 @@ Template Name: Base
 
 <?php get_header(); ?>
 
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+    <div class=".container__header .header .header-title"><h2><?php the_title(); ?></h2></div>
+
+    <div>
+
+    <?php the_content(); ?>
+
+    </div>
+
+    <?php endwhile; ?>
+
+    <?php endif; ?>
 
 <?php get_footer(); ?>
+
