69ca73cf62749df5932551fa7e757b284f6ade68
Dessaint <franck.dessaint@gmail.com>
2016-04-19 17:55:39 +0200

    Edition des pages BTS

--- wp-content/themes/indsf/index.php
+++ wp-content/themes/indsf/index.php
@@ -169,18 +169,3 @@ Template Name: Homepage
 <!-- FOOTER -->
 <?php get_footer(); ?>
 <!-- /FOOTER -->
-	 	</div>
-	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.js"></script>
-
-    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    	<script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
-		<div id="fb-root"></div>
-		<script>(function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) return;
-		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.5";
-		fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));</script>
-	</body>
-</html>
